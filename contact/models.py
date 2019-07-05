d2f75824a3729b4c06249647cf2d06812899f761
Behshad Ghorbani <behshad.ghorbani@truescape.com>
2019-07-05 14:17:42 +1200

    contact form added

--- contact/models.py
+++ contact/models.py
@@ -1,22 +1,33 @@
 from django.db import models
-
-from wagtail.core.models import Page
-from wagtail.admin.edit_handlers import FieldPanel
+from modelcluster.fields import ParentalKey
+from wagtail.admin.edit_handlers import (
+    FieldPanel, FieldRowPanel,
+    InlinePanel, MultiFieldPanel
+)
 from wagtail.core.fields import RichTextField
+from wagtail.contrib.forms.models import AbstractEmailForm, AbstractFormField
+
+from wagtail.contrib.forms.edit_handlers import FormSubmissionsPanel
 
 
-class ContactPage(Page):
+class FormField(AbstractFormField):
+    page = ParentalKey('ContactPage', on_delete=models.CASCADE, related_name='form_fields')
 
-    templates = 'home/home_page.html'
-    header = models.CharField(max_length=100, blank=True, null=True)
-    body = RichTextField(blank=True, null=True)
 
-    content_panels = Page.content_panels + [
-        FieldPanel('header'),
-        FieldPanel('body')
+class ContactPage(AbstractEmailForm):
+    intro = RichTextField(blank=True)
+    thank_you_text = RichTextField(blank=True)
+
+    content_panels = AbstractEmailForm.content_panels + [
+        FormSubmissionsPanel(),
+        FieldPanel('intro', classname="full"),
+        InlinePanel('form_fields', label="Form fields"),
+        FieldPanel('thank_you_text', classname="full"),
+        MultiFieldPanel([
+            FieldRowPanel([
+                FieldPanel('from_address', classname="col6"),
+                FieldPanel('to_address', classname="col6"),
+            ]),
+            FieldPanel('subject'),
+        ], "Email"),
     ]
-
-    class Meta:
-        verbose_name = "Contact Page"
-        verbose_name_plural = "Contact Pages"
-
