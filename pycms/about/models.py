1bd374f055dcbb1c4cbf33966a603703528f36f7
Behshad Ghorbani <behshad.ghorbani@truescape.com>
2019-07-17 14:29:19 +1200

    struct block added plus more

--- pycms/about/models.py
+++ pycms/about/models.py
@@ -1,21 +1,46 @@
 from django.db import models
 
 from wagtail.core.models import Page, Orderable
-from wagtail.admin.edit_handlers import FieldPanel, InlinePanel
-from wagtail.core.fields import RichTextField
+from wagtail.admin.edit_handlers import FieldPanel, InlinePanel, StreamFieldPanel
+from wagtail.core.fields import RichTextField, StreamField
 
 from modelcluster.fields import ParentalKey
 
 from wagtail.images.edit_handlers import ImageChooserPanel
-
+from wagtail.core import blocks
+from wagtail.images.blocks import ImageChooserBlock
+from wagtail.documents.blocks import DocumentChooserBlock
 
 class AboutPage(Page):
+
+    class ImageBlock(blocks.StructBlock):
+        image = ImageChooserBlock(required=True)
+        caption = blocks.CharBlock(required=False)
+        class Meta:
+            icon = 'image'
 
     templates = 'about/about_page.html'
     header = models.CharField(max_length=100, blank=True, null=True)
     body = RichTextField(blank=True, null=True)
 
+    selection = StreamField([
+        ('heading', blocks.CharBlock(label="Some cool heading")),
+        ('structblock', ImageBlock()),
+        ('number', blocks.IntegerBlock()),
+        ('Choice', blocks.ChoiceBlock(choices=[
+            ('tea', 'Tea'),
+            ('coffee', 'Coffee'),
+        ], icon='cup')),
+        ('document', DocumentChooserBlock()),
+        ('url', blocks.URLBlock()),
+        ('date', blocks.DateBlock()),
+        ('time', blocks.TimeBlock()),
+        ('paragraph', blocks.RichTextBlock()),
+        ('image', ImageChooserBlock()),
+    ], blank=True)
+
     content_panels = Page.content_panels + [
+        StreamFieldPanel('selection'),
         FieldPanel('header'),
         FieldPanel('body'),
         InlinePanel('gallery_images', label="Gallery images")
